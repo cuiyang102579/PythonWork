accca5d55c7558b27814249e4310ce96d56691a7
cuiyang <cuiyang@homedo.com>
2020-07-21 17:10:34 +0800

    test

--- testjenkins.py
+++ testjenkins.py
@@ -1,5 +1,10 @@
 import argparse
 
+from common.Log import Log
+
+log = Log()
+log.info("--测试开始--")
+
 parser=argparse.ArgumentParser()
 parser.add_argument("package")
 parser.add_argument("totalEvent")
@@ -9,3 +14,4 @@ v={}
 for key,value in param.items():
     v[key]=value
 print(v)
+log.info(v)
